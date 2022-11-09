009ef66afb4111cacdca4808891c9455aedb8854
shaoming <xsm90827@gmail.com>
2022-11-09 15:28:12 -0600

    test

--- distance_vector_node.py
+++ distance_vector_node.py
@@ -1,23 +1,125 @@
+import copy
+import json
+import math
+
 from simulator.node import Node
+
+
+class Distance_Vector:
+    def __init__(self, cost: int, path: list):
+        self.cost = cost
+        self.path = path
+
+    def __eq__(self, another):
+        return self.cost == another.cost and self.path == another.path
 
 
 class Distance_Vector_Node(Node):
     def __init__(self, id):
         super().__init__(id)
+        self.distance_vectors_self = {}
+        self.distance_vectors_neighbor = {}
+        self.sequence_number = 0
+        self.neighbor_link_cost = {}
+        self.neighbor_sequence_numbers = {}
 
-    # Return a string
-    def __str__(self):
-        return "Rewrite this function to define your node dump printout"
+    def __str__(self): #done
+        message = {}
+        message["self_sequence_number"] = self.sequence_number
+        self.sequence_number += 1
+        for dst, val in self.distance_vectors_self.items():
+            dict = {}
+            dict["path"] = val.path
+            dict["cost"] = val.cost
+            message[dst] = dict
+        return json.dumps(message)
 
-    # Fill in this function
     def link_has_been_updated(self, neighbor, latency):
-        # latency = -1 if delete a link
-        pass
+        if latency == -1:
+            self.delete_link(neighbor)
+        else:
+            self.neighbor_link_cost[neighbor] = latency
+        self.update_dvs()
 
+    def delete_link(self,neighbor: int):
+        self.neighbor_link_cost.pop(neighbor)
+        self.distance_vectors_neighbor.pop(neighbor)
+        self.distance_vectors_self.pop(neighbor)
+
+    def update_dvs(self):
+        old_dvs = copy.deepcopy(self.distance_vectors_self)
+        self.distance_vectors_self = {}
+        self.update_self_neighbour()
+        for source, dvs in self.distance_vectors_neighbor.items():
+            for destination, dv in dvs.items():
+                if source in self.neighbor_link_cost:
+                    new_cost = self.neighbor_link_cost[source] + dv.cost
+                    if destination not in self.distance_vectors_self:
+                        self.update_cost(destination,dv,new_cost)
+                    if destination in self.distance_vectors_self and new_cost < self.distance_vectors_self[destination].cost:
+                        self.update_cost(destination, dv, new_cost)
+        if self.distance_vectors_self != old_dvs: #eq
+            self.send_to_neighbors(str(self))
+
+    def update_self_neighbour(self):
+        for neighbor, link_cost in self.neighbor_link_cost.items():
+            path = [self.id, neighbor]
+            self.distance_vectors_self[neighbor] = Distance_Vector(link_cost, path)
+
+    def update_cost(self, dst:int, dv:Distance_Vector, newcost:int):
+        new_path = [self.id] + copy.deepcopy(dv.path)
+        self.distance_vectors_self[dst] = Distance_Vector(newcost, new_path)
     # Fill in this function
-    def process_incoming_routing_message(self, m):
-        pass
 
-    # Return a neighbor, -1 if no path to destination
+
+
+    def process_incoming_routing_message(self, m):
+        distance_vector_neighbor_message,sequence_number = self.processJson(m)
+        flag = False
+        id1 = next(iter(distance_vector_neighbor_message))
+        id2 = "path"
+        neighbor = int(distance_vector_neighbor_message[id1][id2][0])
+        to_delete = []
+        if neighbor not in self.distance_vectors_neighbor:
+            self.distance_vectors_neighbor[neighbor] = {}
+        if neighbor in self.neighbor_sequence_numbers:
+            if sequence_number < self.neighbor_sequence_numbers[neighbor]:
+                return
+        for dst, value in self.distance_vectors_neighbor[neighbor].items():
+            if(str(dst) not in distance_vector_neighbor_message):
+                to_delete.append(dst)
+        for dst_str, value in distance_vector_neighbor_message.items():
+            dst = int(dst_str)
+            link = Distance_Vector(value['cost'], value['path'])
+
+            looped = (self.id in link.path)
+            if(not ((dst not in self.distance_vectors_neighbor[neighbor]) and looped)):
+                flag = True
+            if (dst in self.distance_vectors_neighbor[neighbor]):
+                if(looped):
+                    self.distance_vectors_neighbor[neighbor].pop(dst) #distance_vectors_neighbor is the dict of dict
+                else:
+                    self.distance_vectors_neighbor[neighbor][dst] = link
+            else:
+                if(not looped):
+                    self.distance_vectors_neighbor[neighbor][dst] = link
+
+
+        if(len(to_delete) > 0):
+            flag = True
+        for dst in to_delete:
+            self.distance_vectors_neighbor[neighbor].pop(dst)
+        self.neighbor_sequence_numbers[neighbor] = sequence_number
+        if flag:
+            self.update_dvs()
+
+    def processJson(self,m):
+        distance_vector_neighbor_message = json.loads(m)
+        sequence_number = distance_vector_neighbor_message["self_sequence_number"]
+        distance_vector_neighbor_message.pop('self_sequence_number')
+        return distance_vector_neighbor_message,sequence_number
+
     def get_next_hop(self, destination):
+        if destination in self.distance_vectors_self:
+            return self.distance_vectors_self[destination].path[1]
         return -1
--- link_state_node.py
+++ link_state_node.py
@@ -1,23 +1,125 @@
+import collections
+import heapq
+import json
+import sys
+import heapq as heap
+
 from simulator.node import Node
 
 
+class Edges:
+    def __init__(self, latency: int, seq_num: int):
+        self.latency = latency
+        self.seq_num = seq_num
+class Nodes:
+    def __init__(self, nodes_id: int):
+        self.nodes_id = nodes_id
+        # key: neighbor, value: cost
+        self.neighbors = {}
+        self.dist = sys.maxsize
+    def addNeighbors(self, neighbor_id:int, latency:int):
+        # if neighbor_id not in self.neighbors.keys():
+        self.neighbors[neighbor_id] = latency
+    def __lt__(self, nodes_two):
+        return self.dist < nodes_two.dist
 class Link_State_Node(Node):
     def __init__(self, id):
         super().__init__(id)
+        self.edges = {}
+        self.nodes = {}
 
     # Return a string
     def __str__(self):
-        return "Rewrite this function to define your node dump printout"
+        message = {}
+        for edge, value in self.edges.items():
+            cur = tuple(edge)
+            newEdges = str(cur)
+            if cur[0] == self.id:
+                newEdges = str((cur[1], cur[0]))
+            message[newEdges] = {
+                                    "latency": value.latency,
+                                    "seq_num": value.seq_num
+                                }
+        return json.dumps(message)
 
     # Fill in this function
     def link_has_been_updated(self, neighbor, latency):
-        # latency = -1 if delete a link
-        pass
-
+        self.edges[frozenset((self.id, neighbor))] = Edges(latency, self.get_time())
+        self.send_to_neighbors(str(self))
     # Fill in this function
     def process_incoming_routing_message(self, m):
-        pass
-
+        should_update = False
+        new_edges = json.loads(m)
+        for cur_key, value in new_edges.items():
+            key_array = cur_key[1:-1].split(',')
+            key_array_pair = (int(key_array[0]), int(key_array[1]))
+            key = tuple(key_array_pair)
+            if self.check_edge(source=key[0], destination=key[1], new_edge=Edges(int(value["latency"]), int(value["seq_num"]))):
+                should_update = True
+        if should_update:
+            self.send_to_neighbors(str(self))
     # Return a neighbor, -1 if no path to destination
     def get_next_hop(self, destination):
-        return -1
+        prev, dist = self.dijkstra()
+        cur_vertice = destination
+        next_hop = None
+        while cur_vertice != self.id:
+            next_hop = cur_vertice
+            if cur_vertice not in prev.keys() or prev[cur_vertice] is None:
+                return -1
+            cur_vertice = prev[cur_vertice]
+        return next_hop
+
+    def check_edge(self, source: int, destination: int, new_edge: Edges) -> bool:
+        if (frozenset((source, destination)) in self.edges and new_edge.seq_num >
+                self.edges[frozenset((source, destination))].seq_num) or frozenset((
+                source, destination)) not in self.edges:
+            self.edges[frozenset((source, destination))] = Edges(new_edge.latency, new_edge.seq_num)
+            return True
+        return False
+
+    # build nodes
+    def dijkstra_preprocessing(self):
+        self.nodes = {}
+        for frozen_edge, val in self.edges.items():
+
+            if val.latency < 0:
+                continue
+            frozen_edge = tuple(frozen_edge)
+            edge_source = frozen_edge[0]
+            edge_destination = frozen_edge[1]
+            if edge_source not in self.nodes.keys():
+                self.nodes[edge_source] = Nodes(edge_source)
+            if edge_destination not in self.nodes.keys():
+                self.nodes[edge_destination] = Nodes(edge_destination)
+            self.nodes[edge_destination].addNeighbors(edge_source, val.latency)
+            self.nodes[edge_source].addNeighbors(edge_destination, val.latency)
+    def dijkstra(self):
+        self.dijkstra_preprocessing()
+        visited = set()
+        prev = {}
+        pq = []
+        dist = collections.defaultdict(lambda: sys.maxsize)
+        dist[self.id] = 0
+        self.nodes[self.id].dist = 0
+        heapq.heappush(pq, self.nodes[self.id])
+
+        while pq:
+            cur_node = heapq.heappop(pq)
+            if cur_node.nodes_id in visited:
+                continue
+            visited.add(cur_node.nodes_id)
+
+            for id, latency in cur_node.neighbors.items():
+                if id in visited:
+                    continue
+                newLatency = dist[cur_node.nodes_id] + latency
+                if dist[id] > newLatency:
+                    prev[id] = cur_node.nodes_id
+                    dist[id] = newLatency
+                    new_node = Nodes(id)
+                    new_node.neighbors = self.nodes[id].neighbors
+                    new_node.dist = newLatency
+                    heapq.heappush(pq, new_node)
+
+        return prev, dist
